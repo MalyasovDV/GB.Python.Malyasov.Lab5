7a0815de463419dddf9577b244ad6930bc30ae59
Dmitry_Malyasov <dmalyasov24@gmail.com>
2023-02-17 17:49:30 +0700

    task26 done

---
+++ task26.py
@@ -0,0 +1,12 @@
+def power(a,b):
+    if (b == 0):
+        return 1
+    return (a * power(a, b - 1))
+
+
+print("Type in the base a")
+a = int(input())
+print("Type in the exponent b")
+b = int(input())
+
+print(f"{a} to the power of {b} is: {power(a,b)}")
