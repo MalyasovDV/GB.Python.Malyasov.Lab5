ae3fe979294e70fcbf2fc32a7ffd485ee68c46c4
Dmitry_Malyasov <dmalyasov24@gmail.com>
2023-02-17 18:57:29 +0700

    doptask done

---
+++ doptask9.py
@@ -0,0 +1,25 @@
+def sum_of_positives(numbers):
+    sum = 0
+    for elem in numbers:
+        if (elem > 0):
+            sum += elem
+    return sum
+
+def composition(numbers):
+    composition = 1
+    for i in range(numbers.index(min(numbers)) + 1, numbers.index(max(numbers))):
+        composition *= numbers[i]
+        print(composition)
+    return composition
+
+file1 = open("input.txt", "r")
+lines = file1.readlines()
+file1.close()
+
+n = int(lines[0])
+numbers = [int(elem) for elem in lines[1].split(" ")]
+
+print(f"{n}\n{numbers}")
+
+print(f"sum : {sum_of_positives(numbers)}\ncomposition between {min(numbers)} and {max(numbers)} : {composition(numbers)}")
+
